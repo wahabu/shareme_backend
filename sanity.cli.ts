ad1e214fc4fb78fd5a0edf241e5a03aa601ade16
wahabu <ritscg2hibo@gmail.com>
2023-04-15 00:18:03 +0900

    Created new repo

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '0p2jft8o',
+    dataset: 'production'
+  }
+})
---
+++ schemas/user.js
@@ -0,0 +1,18 @@
+// schemas/user.js
+export default {
+  name: 'user',
+  type: 'document',
+	title: 'User',
+  fields: [
+    {
+      name: 'userName',
+      type: 'string',
+      title: 'UserName'
+    },
+    {
+      name: 'image',
+      type: 'string',
+      title: 'Image'
+    }
+  ]
+}
